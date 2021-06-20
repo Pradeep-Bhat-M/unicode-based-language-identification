829deef04171dc7af2459bf3f577924a9d2fcd4e
Riyo <pradeepbhatm25@gmail.com>
2021-06-21 00:43:46 +0530

    pulling stuff

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import re
 
-_blocks = []
+blocks = []
 
 def block(ch):
-    assert isinstance(ch, str) and len(ch) == 1, repr(ch)
-    cp = ord(ch)
-    for start, end, name in _blocks:
-        if start <= cp <= end:
+    unicode = ord(ch)
+    for start, end, name in blocks:
+        if start <= unicode <= end:
             return name
 
 
-def _initBlocks(code_file):
+def initBlocks(code_file):
 
     file = open(code_file)
     text = file.read()
@@ -20,9 +19,9 @@ def _initBlocks(code_file):
         m = pattern.match(line)
         if m:
             start, end, name = m.groups()
-            _blocks.append((int(start, 16), int(end, 16), name))
+            blocks.append((int(start, 16), int(end, 16), name))
 
-_initBlocks("unicodes.txt")
+initBlocks("unicodes.txt")
 
 
 
