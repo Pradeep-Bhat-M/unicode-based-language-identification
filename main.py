919ed242c84336ace2cc84a5c8af5eac87f5764c
Riyo <pradeepbhatm25@gmail.com>
2021-06-21 01:29:25 +0530

    Nicely Commented

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ import re
 
 blocks = []
 languages_dict = {}
+past_lang = (65, 122, 'English')
 
-def parseInput():
-    input_file = open("200.txt",encoding="utf8")
-    data = input_file.read()
+def parseInput(inputFile):
+    inFile = open(inputFile, encoding="utf8")
+    data = inFile.read()
     data = re.sub(' +', ' ', data)                  # Removing redundant multiple spaces
-    data = re.sub('\n+','\n',data)                  # Removing extra lines
+    data = re.sub('\n+','\n', data)                 # Removing extra lines
     return(data.split(' '))
 
 
-def addInDictionary(language):
+def addInDictionary(language):                      # Adding words into a dictionary
     if language in languages_dict:
         languages_dict.get(language).append(word)
     else:
         languages_dict[language] = [word]
 
-def block(unicode):
+def block(unicode):                                 # Comparing Unicode ranges to find the script
     for start, end, name in blocks:
         if start <= unicode <= end:
             return (start, end, name)
 
-def makeBlocks(code_file):                          # Loading all the unicode ranges
+def makeBlocks(code_file):                          # Loading all the Unicode ranges
 
     file = open(code_file)
     text = file.read()
@@ -36,16 +37,14 @@ def makeBlocks(code_file):                          # Loading all the unicode ra
 
 
 makeBlocks("unicodes.txt")
-past_lang = (65, 122, 'English')
-
-dataByWOrds = parseInput()
+dataByWOrds = parseInput("input.txt")
 
 for word in dataByWOrds:
     unicode = ord(word[0])                          # Fetching the unicode of the character
     if past_lang[0] <= unicode <= past_lang[1]:
         language = past_lang[2]
     else: 
-        past_lang = block(unicode)
+        past_lang = block(unicode)                  # Updating past_lang, so that we need not search the whole unicode block again.
         language = past_lang[2]
 
     addInDictionary(language)
